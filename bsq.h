5af2f6eb39109a6c3a509d76b068c80e043de52a
Jason BONIWELL <jboniwel@e2r5p3.42.fr>
2017-07-26 02:35:50 +0200

    descriptive mesage: the transform array only works for a square, made a jason_main to work with it, added headers to bsq.h

--- bsq.h
+++ bsq.h
@@ -6,11 +6,22 @@
 /*   By: lkinzel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/25 12:19:25 by lkinzel           #+#    #+#             */
-/*   Updated: 2017/07/25 12:20:05 by lkinzel          ###   ########.fr       */
+/*   Updated: 2017/07/26 02:18:27 by jboniwel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BSQ_H
 # define BSQ_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		**make_it_numbers(char *array, int height);
+void	show(int **tab, int height);
+void	fill(int **tab, int height);
+int		transform(char src);
+int		array_is_valid(char *str);
 
 #endif
--- transform_array.c
+++ transform_array.c
@@ -6,39 +6,113 @@
 /*   By: jboniwel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/25 11:58:54 by jboniwel          #+#    #+#             */
-/*   Updated: 2017/07/25 12:20:57 by lkinzel          ###   ########.fr       */
+/*   Updated: 2017/07/26 02:28:07 by jboniwel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
 
-int		array_is_valid(char **tab)
+int		array_is_valid(char *str)
+{
+	int i;
+	int j;
+	int k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (str[k] != '\n')
+		k++;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\n')
+			j++;
+		i++;
+	}
+	if (k == j)
+		return (1);
+	else
+		return (0);
+}
+
+int		transform(char src)
+{
+	if (src == '.')
+		return (0);
+	else
+		return (1);
+}
+
+void	fill(int **tab, int height)
 {
 	int i;
 	int j;
 
 	i = 0;
 	j = 0;
-	while (tab[1][i] != '\n')
+	while (++j < height)
+		tab[i][j] += tab[i][j - 1];
+	j = 0;
+	i = 1;
+	while (i < height)
+	{
+		tab[i][j] += tab[i - 1][j];
 		i++;
-	while (tab[j] != 0)
-		j++;
-	if (i == j)
-		return (1);
-	else
-		return (0);
+	}
+	i = 1;
+	while (i < height)
+	{
+		j = 1;
+		while (j < height)
+		{
+			tab[i][j] += tab[i - 1][j] + tab[i][j - 1] - tab[i - 1][j - 1];
+			j++;
+		}
+		i++;
+	}
 }
 
-int g_i = 0;
+void	show(int **tab, int height)
+{
+	int i;
+	int j;
 
-int g_j = 0;
+	i = 0;
+	while (i < height)
+	{
+		j = 0;
+		while (j < height)
+		{
+			printf("%d", tab[i][j]);
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+}
 
-int		**make_it_dank(char **array, int height, int length)
+int		**make_it_numbers(char *array, int height)
 {
 	int **tab;
-	
-	if (array_is_valid(array) == 0)
-		ft_putstr("map error\n");
-		return (0);
+	int i;
+	int j;
+	int k;
+
+	i = 0;
+	k = 0;
 	tab = (int**)malloc(height * sizeof(int*));
+	while (array[i] != 0)
+	{
+		j = 0;
+		tab[k] = (int*)malloc(height * sizeof(int));
+		while (array[i] != '\n')
+		{
+			tab[k][j] = transform(array[i]);
+			i++;
+			j++;
+		}
+		k++;
+		i++;
+	}
+	return (tab);
 }
