64dd4c7555da45bd3bc6b0ed10e556adb1eaa558
Linus KINZEL <lkinzel@student.42.fr>
2017-07-26 23:17:47 +0200

    find biggest normed

--- find_biggest.c
+++ find_biggest.c
@@ -6,56 +6,11 @@
 /*   By: lkinzel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 00:08:37 by lkinzel           #+#    #+#             */
-/*   Updated: 2017/07/26 22:51:17 by lkinzel          ###   ########.fr       */
+/*   Updated: 2017/07/26 23:09:10 by lkinzel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
-
-int		is_valid(int **arr, int startingpos[2], int size)
-{
-	size--;
-	if ((startingpos[0] + size >= g_height) ||
-			(startingpos[1] + size >= g_length))
-		return (0);
-	if ((arr[startingpos[0] + size][startingpos[1] + size]
-				- arr[startingpos[0] + size][startingpos[1] - 1]
-				- arr[startingpos[0] - 1][startingpos[1] + size]
-				+ arr[startingpos[0] - 1][startingpos[1] - 1]) > 0)
-		return (0);
-	else
-		return (1);
-}
-
-int		brute_force_valid(int **arr, int startingpos[2], int size)
-{
-	int row;
-	int col;
-
-	row = startingpos[0];
-	col = startingpos[1];
-	if (size == 1)
-	{
-		if (arr[row][col] == 0)
-			return (1);
-	}
-	else
-	{
-		while (row < startingpos[0] + size)
-		{
-			col = startingpos[1];
-			while (col < startingpos[1] + size)
-			{
-				if (arr[row][col] != 0)
-					return (0);
-				col++;
-			}
-			row++;
-		}
-		return (1);
-	}
-	return (0);
-}
 
 void	checkfirstrow(int **arr)
 {
@@ -86,14 +41,10 @@ void	checkfirstrow(int **arr)
 	}
 }
 
-void	checkfirstcol(int **arr)
+void	checkfirstcol(int **arr, int con, int onthisrow, int foundfirst)
 {
-	int con;
-	int onthisrow;
 	int startingpos[2];
-	int foundfirst;
 
-	foundfirst = 0;
 	startingpos[0] = 1;
 	startingpos[1] = 0;
 	while (startingpos[0] + g_dim < g_length)
@@ -101,10 +52,9 @@ void	checkfirstcol(int **arr)
 		con = 1;
 		while (con)
 		{
+			onthisrow = 1;
 			if ((g_pos[0] == startingpos[0]) && (!foundfirst))
 				onthisrow = 0;
-			else
-				onthisrow = 1;
 			if (brute_force_valid(arr, startingpos, g_dim + onthisrow))
 			{
 				if (onthisrow)
@@ -120,17 +70,12 @@ void	checkfirstcol(int **arr)
 	}
 }
 
-void	find_biggest(int **arr, char *str_no_information)
+void	checkmain(int **arr, int con)
 {
 	int	startingpos[2];
-	int **tab;
-	int con;
 
 	startingpos[0] = 1;
 	startingpos[1] = 1;
-	g_pos[0] = 0;
-	g_pos[1] = 0;
-	g_dim = 0;
 	while (startingpos[0] + g_dim < g_height)
 	{
 		startingpos[1] = 1;
@@ -152,9 +97,17 @@ void	find_biggest(int **arr, char *str_no_information)
 		}
 		startingpos[0]++;
 	}
+}
+
+void	find_biggest(int **arr, char *str_no_information)
+{
+	int **tab;
+
+	g_pos[0] = 0;
+	g_pos[1] = 0;
+	g_dim = 0;
+	checkmain(arr, 1);
 	make_it_numbers(&tab, str_no_information, g_height, 0);
 	checkfirstrow(tab);
-	checkfirstcol(tab);
-	printf("g_pos: %d, %d\n", g_pos[0], g_pos[1]);
-	printf("g_dim: %d\n", g_dim);
+	checkfirstcol(tab, 1, 0, 0);
 }
